ba0022e0350c14fe6447a324a39ce1006d909b03
lbastoss <bastosls17@gmail.com>
2024-12-01 17:07:44 -0300

    finish

---
+++ script.js
@@ -0,0 +1,24 @@
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minuties')
+const seconds = document.getElementById('seconds')
+
+
+const clock = setInterval(function time() {
+
+    let dataToday = new Date()
+    let hor = dataToday.getHours()
+    let min = dataToday.getMinutes()
+    let sec = dataToday.getSeconds()
+
+
+    if (hor < 10) hor = '0' + hor;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + hor;
+
+
+
+    hours.textContent = hor
+    minutes.textContent = min
+    seconds.textContent = sec
+})
+
